edcc5e7cf7f8fa0a889d510ed1c150154d996d1f
ChenLi2000 <lichen@riashitas-MacBook-Air.local>
2016-11-16 09:19:41 -0500

    Corrected print statement

--- src/com/cooldante/YayDante.java
+++ src/com/cooldante/YayDante.java
@@ -3,7 +3,7 @@ package com.cooldante;
 public class YayDante {
 	public static void main(String[] args) {
 		
-		System.out.println("Echo is cool");
+		System.out.println("Echo is boring");
 		//Added comment lfeeowifjqeoi
 		
 	}
